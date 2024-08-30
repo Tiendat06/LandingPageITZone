759a17aebcf2e847f5393bb15894ec380e2501c5
TienDat <tadat290903@gmail.com>
2024-08-30 23:21:33 +0700

    update a little bit of activity page

--- views/site/activity.php
+++ views/site/activity.php
@@ -9,7 +9,7 @@
         </div>
     </section>
     <section class="activity-menu">
-        <div class="activity-menu__list">
+        <div class="activity-menu__list col-lg-12 col-md-12 col-sm-12">
             <div class="activity-menu__item">
                 <div class="activity-menu__logo">
                     <img class="activity-menu__img" src="/public/img/home/activity_4.png" alt="">
@@ -29,8 +29,31 @@
                 <a class="activity-menu__para">Hoạt động được tài trợ</a>
             </div>
         </div>
+        <div class="activity-menu__border col-lg-9 col-md-9 col-sm-9"></div>
     </section>
-    <section class="activity-local"></section>
+    <section class="activity-local">
+        <div class="activity-local__header">
+            <h1 class="activity-local__title">HOẠT ĐỘNG NỘI BỘ</h1>
+        </div>
+        <div class="activity-local__text">
+            <p class="activity-local__para">Các sự kiện được CLB tổ chức hết sức đa dạng từ các cuộc thi đến các hoạt động ngoại khóa.</p>
+            <p class="activity-local__para">Thành viên CLB sẽ luôn được trau dồi, ôn tập kiến thức và gắn kết với nhau hơn như một đại gia đình.</p>
+        </div>
+        <div class="activity-local__first">
+            <div class="activity-local__logo--outer col-lg-6 col-md-12 col-sm-12">
+                <div class="activity-local__logo">
+                    <div class="activity-local__frame">
+                        <img src="/public/img/activity/activity_1.png" alt="" srcset="" class="activity-local__img">
+                    </div>
+                </div>
+            </div>
+
+            <div class="activity-local__text col-lg-6 col-md-12 col-sm-12">
+                <h5 class="activity-local__text-title">Hoạt động 1</h5>
+                <p class="activity-local__text-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, cumque dolores eligendi explicabo fuga in incidunt iure laboriosam libero nesciunt nostrum qui, sequi suscipit tempora tenetur ullam veritatis vero vitae.</p>
+            </div>
+        </div>
+    </section>
     <section class="activity-school"></section>
     <section class="activity-cost"></section>
 </div>
