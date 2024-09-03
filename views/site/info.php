32cf0b697b1ca612ae5dd734527fd93ade266c8b
TienDat <tadat290903@gmail.com>
2024-09-03 15:13:15 +0700

    update page info

--- views/site/info.php
+++ views/site/info.php
@@ -1,3 +1,171 @@
 <?php
 ?>
-<h1>Info</h1>
+<div class="info">
+    <section class="info-intro">
+        <div class="info-intro__bg">
+            <img src="/public/img/about/about-bg.png" alt="" srcset="" class="info-intro__bg-img">
+        </div>
+        <div class="info-intro__text">
+            <h1 class="info-intro__text-title">THÔNG TIN, CƠ CẤU ĐẠI GIA ĐÌNH </h1>
+            <div class="info-intro__text-logo">
+<!--                <img src="/public/img/about/IT-ZONE-text-2.png" alt="" srcset="" class="info-intro__text-img">-->
+            </div>
+        </div>
+    </section>
+    <section class="info-about">
+        <div class="info-about__left col-lg-5 col-md-12 col-sm-12">
+            <h1 class="info-about__left-title">
+                THÔNG TIN CHUNG</br>
+                <span>CLB IT-ZONE</span>
+            </h1>
+            <img src="/public/img/general/logo_ITZone.png" alt="" srcset="" class="info-about__left-img">
+        </div>
+        <div class="info-about__middle col-lg-2 col-md-0 col-sm-0">
+            <div class="info-about__middle-vertical"></div>
+        </div>
+        <div class="info-about__right col-lg-5 col-md-12 col-sm-12">
+            <ul class="info-about__right-list list-unstyled">
+                <li class="info-about__right-item">
+                    <div class="info-about__right-icon col-lg-2 col-md-12 col-sm-12">
+                        <img src="/public/img/icon/icon-person.png" alt="" srcset="" class="info-about__right-icon-item">
+                    </div>
+                    <div class="info-about__right-text col-lg-10 col-md-12 col-sm-12">
+                        <p class="info-about__right-text__title">Tên câu lạc bộ</p>
+                        <p class="info-about__right-text__para">Câu lạc bộ học thuật IT-Zone</p>
+                    </div>
+                </li>
+                <li class="info-about__right-item mt-4">
+                    <div class="info-about__right-icon col-lg-2 col-md-12 col-sm-12">
+                        <img src="/public/img/icon/icon-group.png" alt="" srcset="" class="info-about__right-icon-item">
+                    </div>
+                    <div class="info-about__right-text col-lg-10 col-md-12 col-sm-12">
+                        <p class="info-about__right-text__title">Đơn vị bảo trợ</p>
+                        <p class="info-about__right-text__para">Trung tâm tin học CAIT</p>
+                    </div>
+                </li>
+                <li class="info-about__right-item mt-4">
+                    <div class="info-about__right-icon col-lg-2 col-md-12 col-sm-12">
+                        <img src="/public/img/icon/icon-edit.png" alt="" srcset="" class="info-about__right-icon-item">
+                    </div>
+                    <div class="info-about__right-text col-lg-10 col-md-12 col-sm-12">
+                        <p class="info-about__right-text__title">Châm ngôn</p>
+                        <p class="info-about__right-text__para">“Có Tâm - Có Tầm - Chia sẻ - Hỗ Trợ”</p>
+                    </div>
+                </li>
+                <li class="info-about__right-item mt-4">
+                    <div class="info-about__right-icon col-lg-2 col-md-12 col-sm-12">
+                        <img src="/public/img/icon/icon-location.png" alt="" srcset="" class="info-about__right-icon-item">
+                    </div>
+                    <div class="info-about__right-text col-lg-10 col-md-12 col-sm-12">
+                        <p class="info-about__right-text__title">Quy mô</p>
+                        <p class="info-about__right-text__para">32 thành viên</p>
+                    </div>
+                </li>
+                <li class="info-about__right-item mt-4">
+                    <div class="info-about__right-icon col-lg-2 col-md-12 col-sm-12">
+                        <img src="/public/img/icon/icon-clock.png" alt="" srcset="" class="info-about__right-icon-item">
+                    </div>
+                    <div class="info-about__right-text col-lg-10 col-md-12 col-sm-12">
+                        <p class="info-about__right-text__title">Thời gian hoạt động</p>
+                        <p class="info-about__right-text__para">Thứ 2, 4, 6 - Sáng: 7h30 - 11h, Chiều: 1h30 - 5h</p>
+                    </div>
+                </li>
+                <li class="info-about__right-item mt-4">
+                    <div class="info-about__right-icon col-lg-2 col-md-12 col-sm-12">
+                        <img src="/public/img/icon/icon-place.png" alt="" srcset="" class="info-about__right-icon-item">
+                    </div>
+                    <div class="info-about__right-text col-lg-10 col-md-12 col-sm-12">
+                        <p class="info-about__right-text__title">Địa điểm</p>
+                        <p class="info-about__right-text__para">Sảnh 5AB</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section class="info-structure">
+        <h1 class="info-structure__title">CƠ CẤU TỔ CHỨC <span>CLB IT-ZONE</span></h1>
+        <div class="info-structure__content">
+            <div class="info-structure__item">
+                <div class="info-structure__item-title col-lg-3 col-sm-3 col-md-3">
+                    <p class="info-structure__item-title-stt col-lg-3 col-sm-3 col-md-3">
+                        <span>1</span>
+                    </p>
+                    <p class="info-structure__item-title-subtitle col-lg-9 col-sm-0 col-md-0">
+                        <span>Giảng viên cố vấn</span>
+                    </p>
+                </div>
+                <div class="info-structure__item-text col-lg-9 col-sm-9 col-md-9">
+                    <div class="info-structure__item-text--inner">
+                        <h4 class="info-structure__item-text-subtitle">1. Giảng viên cố vấn Trung tâm CAIT</h4>
+                        <p class="info-structure__item-text-para">Là người đóng vai trò tư vấn và định hướng cho các hoạt động của câu lạc bộ, đảm bảo mọi hoạt động của CLB đều đúng mục tiêu và chiến lược phát triển.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="info-structure__item">
+                <div class="info-structure__item-title info-structure__item-title-bcn-tpm col-lg-3 col-sm-3 col-md-3">
+                    <p class="info-structure__item-title-stt col-lg-3 col-sm-3 col-md-3">
+                        <span>2</span>
+                    </p>
+                    <p class="info-structure__item-title-subtitle col-lg-9 col-sm-0 col-md-0">
+                        <span>Ban chủ nhiệm</span>
+                    </p>
+                </div>
+                <div class="info-structure__item-text col-lg-9 col-sm-9 col-md-9">
+                    <div class="info-structure__item-text--inner">
+                        <h4 class="info-structure__item-text-subtitle">2. Chủ nhiệm CLB</h4>
+                        <p class="info-structure__item-text-para">Là người đứng đầu, chịu trách nhiệm quản lý và điều hành toàn bộ các hoạt động của CLB. Chủ nhiệm có nhiệm vụ đưa ra các quyết định quan trọng và phối hợp với các ban khác để đảm bảo sự phát triển của CLB.</p>
+                    </div>
+                    <div class="info-structure__item-text--inner mt-4">
+                        <h4 class="info-structure__item-text-subtitle">3. Phó Chủ nhiệm CLB</h4>
+                        <p class="info-structure__item-text-para">Hỗ trợ Chủ nhiệm CLB trong việc điều hành và quản lý các hoạt động. Phó Chủ nhiệm thường chịu trách nhiệm giám sát các mảng cụ thể và thay thế Chủ nhiệm khi cần thiết.</p>
+                    </div>
+                    <div class="info-structure__item-text--inner mt-4">
+                        <h4 class="info-structure__item-text-subtitle">4. Trưởng ban Kỹ thuật (KT)</h4>
+                        <p class="info-structure__item-text-para">Quản lý và điều phối các hoạt động liên quan đến kỹ thuật trong CLB, bao gồm xử lý các vấn đề kỹ thuật, hỗ trợ sửa máy tính cho sinh viên tại sảnh 5AB. Ngoài ra, còn đảm nhiệm các dự án và đảm bảo chất lượng các sản phẩm phần mềm trong CLB.</p>
+                    </div>
+                    <div class="info-structure__item-text--inner mt-4">
+                        <h4 class="info-structure__item-text-subtitle">5. Trưởng ban Truyền thông (TT)</h4>
+                        <p class="info-structure__item-text-para">Là Là người đóng vai trò tư vấn và định hướng cho các hoạt động của câu lạc bộ, đảm bảo mọi hoạt động của CLB đều đúng mục tiêu và chiến lược phát triển.</p>
+                    </div>
+                    <div class="info-structure__item-text--inner mt-4">
+                        <h4 class="info-structure__item-text-subtitle">6. Trưởng ban Hành chính - Nhân sự (HC-NS)</h4>
+                        <p class="info-structure__item-text-para">Quản lý các công việc liên quan đến hành chính và nhân sự trong CLB, bao gồm quản lý thành viên và đảm bảo các quy trình hành chính được thực hiện đúng quy định.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="info-structure__item">
+                <div class="info-structure__item-title col-lg-3 col-sm-3 col-md-3">
+                    <p class="info-structure__item-title-stt col-lg-3 col-sm-3 col-md-3">
+                        <span>3</span>
+                    </p>
+                    <p class="info-structure__item-title-subtitle col-lg-9 col-sm-0 col-md-0">
+                        <span>Kỹ thuật viên</span>
+                    </p>
+                </div>
+                <div class="info-structure__item-text col-lg-9 col-sm-9 col-md-9">
+                    <div class="info-structure__item-text--inner">
+                        <h4 class="info-structure__item-text-subtitle">7. Kỹ thuật viên</h4>
+                        <p class="info-structure__item-text-para">Hỗ trợ Trưởng ban Kỹ thuật trong việc xử lý các công việc kỹ thuật, sửa máy tính và giải đáp các vấn đề về công nghệ cho sinh viên tại sảnh 5AB, cũng như tham gia vào các dự án phát triển phần mềm.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="info-structure__item">
+                <div class="info-structure__item-title info-structure__item-title-bcn-tpm col-lg-3 col-sm-3 col-md-3">
+                    <p class="info-structure__item-title-stt col-lg-3 col-sm-3 col-md-3">
+                        <span>4</span>
+                    </p>
+                    <p class="info-structure__item-title-subtitle col-lg-9 col-sm-0 col-md-0">
+                        <span>Tổ phần mềm</span>
+                    </p>
+                </div>
+                <div class="info-structure__item-text col-lg-9 col-sm-9 col-md-9">
+                    <div class="info-structure__item-text--inner">
+                        <h4 class="info-structure__item-text-subtitle">8. Tổ phần mềm</h4>
+                        <p class="info-structure__item-text-para">Chịu trách nhiệm phát triển, quản lý và duy trì các sản phẩm phần mềm của CLB. Tổ này tập trung vào việc lập trình, xây dựng các quy trình phát triển phần mềm dựa vào thực tế, và đảm bảo các dự án phần mềm được hoàn thành đúng tiến độ.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="info-structure__content-vertical"></div>
+        </div>
+    </section>
+</div>
